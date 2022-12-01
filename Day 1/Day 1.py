c1266d36d52a779b87a40e4bbf54ec803016ad7b
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-01 11:39:11 +0100

    Day 1 - both parts have been completed

--- Day 1/Day 1.py
+++ Day 1/Day 1.py
@@ -1,1 +1,18 @@
-print ("hello")
+file = open("input-p.txt", mode = "r")
+snacks = file.read().split("\n")
+
+calories = 0
+all_calories = []
+
+for snack in snacks:
+    if(snack == ""):
+        all_calories.append(calories)
+        calories = 0
+    else:
+        calories = calories + int(snack)
+
+all_calories.append(calories) # The last Elf's snacks
+all_calories.sort(reverse=True)
+
+print("Part One = ", all_calories[0])
+print("Part two = ", all_calories[0] + all_calories[1] + all_calories[2])
