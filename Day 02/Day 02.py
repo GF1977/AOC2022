bf3e1147467dba9ed6fcfbde08ea65fa34ca4e5a
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-02 11:04:48 +0100

    Day 2 - both parts have been completed

--- Day 02/Day 02.py
+++ Day 02/Day 02.py
@@ -11,17 +11,27 @@ myCodes = {"X": "A", "Y": "B", "Z": "C"}
 # n + m  means: n = bonus for lost, draw, won    m =  bonus for rock, paper, scissors
 results = {"AA": 3 + 1, "AB": 6 + 2, "AC": 0 + 3,"BA": 0 + 1,"BB": 3 + 2, "BC": 6 + 3, "CA": 6 + 1, "CB": 0 + 2, "CC": 3 + 3}
 
+
+# X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+strategyGuide = {"AX":"AC", "AY":"AA", "AZ":"AB", "BX":"BA", "BY":"BB", "BZ":"BC", "CX":"CB", "CY":"CC", "CZ":"CA"}
+
 def Game(scenario):
     return results[scenario]
 
-totalScore = 0
+totalScorePartOne = 0
+totalScorePartTwo = 0
 for round in gamePlan:
-    scenario = round[0] + myCodes[round[2]]
-    res = Game(scenario)
-    totalScore = totalScore + res
+    scenarioOne = round[0] + myCodes[round[2]]
+    res = Game(scenarioOne)
+    totalScorePartOne = totalScorePartOne + res
+
+    scenarioTwo = strategyGuide[round[0] + round[2]]
+    res = Game(scenarioTwo)
+    totalScorePartTwo = totalScorePartTwo + res
 
 print("----------------------------------")
-print("Part one:", totalScore)
+print("Part one:", totalScorePartOne)
+print("Part one:", totalScorePartTwo)
 
 
 
