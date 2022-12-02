e7a8bf3047d991ac6211e7f077694f54bfcd0c39
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-02 10:45:00 +0100

    Day 2 - The first part has been completed

---
+++ Day 02/Day 02.py
@@ -0,0 +1,39 @@
+file = open("Day02-Input-p.txt", mode = "r")
+gamePlan = file.read().split("\n")
+
+# A for Rock, B for Paper, and C for Scissors
+# Scores: (1 for Rock, 2 for Paper, and 3 for Scissors)
+# Scores: (0 if you lost, 3 if the round was a draw, and 6 if you won)
+
+figures = {"A": "Rock", "B": "Paper", "C": "Scissors"}
+myCodes = {"X": "A", "Y": "B", "Z": "C"}
+
+
+# first letter from my opponent, second - from me
+# n + m  means: n = lost, draw, won    m = rock, paper, scissors bonus
+results = {"AA": 3 + 1, "AB": 6 + 2, "AC": 0 + 3,"BA": 0 + 1,"BB": 3 + 2, "BC": 6 + 3, "CA": 6 + 1, "CB": 0 + 2, "CC": 3 + 3}
+
+def Game(scenario):
+    print(scenario)
+    print("Elf:", figures[scenario[0]], "  -  Me:", figures[scenario[1]])
+    return results[scenario]
+
+totalScore = 0
+for round in gamePlan:
+    PlayersCombination = round.split(" ")
+    myCombination = myCodes[PlayersCombination[1]]
+    scenario = PlayersCombination[0] + myCombination
+    print(round)
+    res = Game(scenario)
+    totalScore = totalScore + res
+    print(res)
+
+print("----------------------------------")
+print("Total Score:", totalScore)
+
+
+
+
+
+
+
