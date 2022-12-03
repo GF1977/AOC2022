0646be941f4d455f74a42c5ccd864c00dd346cc8
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-03 12:00:16 +0100

    Day 1 - file names adjusting

--- Day 01/Day 01.py
+++ Day 01/Day 01.py
@@ -1,4 +1,4 @@
-file = open("input-p.txt", mode = "r")
+file = open("Day01-Input-p.txt", mode ="r")
 snacks = file.read().split("\n")
 
 calories = 0
@@ -16,3 +16,7 @@ all_calories.sort(reverse=True)
 
 print("Part One = ", all_calories[0])
 print("Part two = ", all_calories[0] + all_calories[1] + all_calories[2])
+
+# Answers:
+# Part One =  68787
+# Part two =  198041
