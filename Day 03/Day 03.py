be6890eadcba9b4609eb9f0c61f41066d812db74
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-03 11:13:22 +0100

    Day 3 - part one has been done

--- Day 03/Day 03.py
+++ Day 03/Day 03.py
@@ -1,32 +1,35 @@
-file = open("Day03-input-p.txt", mode ="r")
+file = open("Day03-input-p.txt", mode="r")
 rucksacks = file.read().split("\n")
 
 answerPartOne = 0
 answerPartTwo = 0
+
+
 def GetPriority(letter):
     ASCIIcode = ord(letter)
-    if(ASCIIcode < 91 ): # A-Z  are 65 - 91, all of them less than 91
+    if (ASCIIcode < 91):  # A-Z  are 65 - 91, all of them are less then 91
         return ASCIIcode - 38  # A = 65
     else:
         return ASCIIcode - 96  # a = 97
 
-def GetCommonItem(leftC, rightC):
-    for itemFromLeft in leftC:
-        for itemFromRight in rightC:
-            if itemFromLeft == itemFromRight:
-                return itemFromLeft
-    return -1
 
+def GetCommonItem(A, B):
+    result = []
+    for a in A:
+        for b in B:
+            if a == b:
+                result.append(a)
+    return result
 
 for rucksack in rucksacks:
-    compartmentSize = int(len(rucksack)/2)
-    #print(compartmentSize)
-    leftC  = rucksack[:compartmentSize]
+    compartmentSize = int(len(rucksack) / 2)
+    # print(compartmentSize)
+    leftC = rucksack[:compartmentSize]
     rightC = rucksack[compartmentSize:]
-    #print(leftC,":",rightC)
-    commonItem = GetCommonItem(leftC, rightC)
+    # print(leftC,":",rightC)
+    commonItem = GetCommonItem(leftC, rightC)[0]
     answerPartOne = answerPartOne + GetPriority(commonItem)
-    print(commonItem, " = ", GetPriority(commonItem))
+    # print(commonItem, " = ", GetPriority(commonItem))
 
 print("----------------------------")
 print("Part one:", answerPartOne)
@@ -35,9 +38,3 @@ print("Part one:", answerPartTwo)
 # Answers:
 # Part one: 7674
 # Part one:
-
-
-
-
-
-
