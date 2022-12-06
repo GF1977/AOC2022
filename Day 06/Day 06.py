124805892f8c8a97a8c2f1febf9a5eb543d5e0bc
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-06 11:04:53 +0100

    Day 06 - empty project

---
+++ Day 06/Day 06.py
@@ -0,0 +1,18 @@
+def parse_file(file_to_process):
+    file = open(file_to_process, mode="r")
+    data: list[str] = file.read().split("\n")
+
+    return 0
+
+
+file_name = "Day06-input-p.txt"
+part_one = 0
+part_two = 1
+
+print("----------------------------")
+print("Part One:", part_one)
+print("Part Two:", part_two)
+
+# Answers:
+# Part One: RFFFWBPNS
+# Part Two: CQQBBJFCS
