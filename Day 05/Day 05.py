ebf05fc398e964d16fc7b5d8f4ee44db802b389f
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-06 11:00:58 +0100

    Day 5 - optimized array conversion (rows to columns)

--- Day 05/Day 05.py
+++ Day 05/Day 05.py
@@ -2,33 +2,21 @@ def parse_file(file_to_process):
     file = open(file_to_process, mode="r")
     data: list[str] = file.read().split("\n")
 
-    stacks = []
+    stacks = [[]]
     commands = []
-    # process first part of the file, get containers into stacks
+
+    # converting rows to columns
     for line in data:
         if line == "":
             break
-        # adding '*' to fill empty positions, which are _left_ from the last container
-        # **A  or *******BCD, but not **A****** or ******XYZ**
         simplified_line = line.replace("    ", "[*] ").replace(" ", "").replace("[", "").replace("]", "")
-        stacks.append(simplified_line)
-
-    # here we add '*' to fill the empty positions on the right side, the len of each line will be the same
-    stacks_adjusted_by_width = []
-    for stack in stacks:
-        a = len(max(stacks)) - len(stack)
-        stacks_adjusted_by_width.append(stack + "*" * a)
-
-    # rotate the array to get each stack as individual list
-    # A B       1 A
-    # 1 2       2 B
-    tmp = list(zip(*stacks_adjusted_by_width.__reversed__()))
-    stacks = []
-    for line in tmp:
-        line = list(line)
-        while line.__contains__("*"):
-            line.remove("*")
-        stacks.append(line)
+        index = 0
+        for symbol in simplified_line:
+            if len(stacks) <= index:
+                stacks.append([])
+            if symbol != "*":
+                stacks[index].insert(0, symbol)
+            index = index + 1
 
     # get commands
     for line in data:
@@ -72,7 +60,8 @@ def run_crate_mover(input_data, crane_model):
         answer = answer + stack[-1]
     return answer
 
-file_name = "Day05-input-d.txt"
+
+file_name = "Day05-input-p.txt"
 part_one = run_crate_mover(file_name, 9000)
 part_two = run_crate_mover(file_name, 9001)
 
