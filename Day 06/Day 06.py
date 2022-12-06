172c22403b904ea145272c69d7685fd744715773
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-06 12:05:06 +0100

    Day 06 - both parts completed

--- Day 06/Day 06.py
+++ Day 06/Day 06.py
@@ -2,12 +2,37 @@ def parse_file(file_to_process):
     file = open(file_to_process, mode="r")
     data: list[str] = file.read().split("\n")
 
-    return 0
+    return data
+
+
+def is_message_unique(msg):
+    my_dict = {}
+    for symbol in msg:
+        if symbol not in my_dict:
+            my_dict[symbol] = 0
+
+    return len(my_dict) == len(msg)
+
+
+def get_marker(msg, shift):
+    index = shift
+    for i in range(0, len(msg)):
+        sub_message = msg[i:i + shift]
+        res = is_message_unique(sub_message)
+        if res:
+            break
+        else:
+            index = index + 1
+
+    return index
 
 
 file_name = "Day06-input-p.txt"
-part_one = 0
-part_two = 1
+buffer = parse_file(file_name)
+for message in buffer:
+    part_one = get_marker(message, 4)
+    part_two = get_marker(message, 14)
+
 
 print("----------------------------")
 print("Part One:", part_one)
