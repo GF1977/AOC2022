2fd9a4164dc2177a8c332b0029455613af78d093
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-10 22:31:30 +0100

    Day 09 - solved! Now need to optimize

--- Day 09/Day 09.py
+++ Day 09/Day 09.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Rope:
     def __init__(self):
         self.head_x, self.head_y, self.tail_x, self.tail_y = 0, 0, 0, 0
@@ -18,13 +21,32 @@ class Rope:
         self.head_y += step_y
 
     def move_tail(self):
-        s_x = self.prev_pos_head_x - self.tail_x
-        s_y = self.prev_pos_head_y - self.tail_y
+        # s_x = self.prev_pos_head_x - self.tail_x
+        # s_y = self.prev_pos_head_y - self.tail_y
+        #
+        # self.tail_x = self.prev_pos_head_x
+        # self.tail_y = self.prev_pos_head_y
 
-        self.tail_x = self.prev_pos_head_x
-        self.tail_y = self.prev_pos_head_y
+        if self.head_x > self.tail_x:
+            s_x = 1
+        else:
+            s_x = -1
 
-        return s_x, s_y
+        if self.head_x == self.tail_x:
+            s_x = 0
+
+
+        if self.head_y > self.tail_y:
+            s_y = 1
+        else:
+            s_y = -1
+
+        if self.head_y == self.tail_y:
+            s_y = 0
+
+        self.tail_x += s_x
+        self.tail_y += s_y
+        #return s_x, s_y
 
     def get_visited_positions(self):
         return len(self.where_tail_was)
@@ -51,14 +73,20 @@ def emulate_rope_motion(command, ropes):
     if direction == "D":
         step_y = -1
 
-
-
     for step in range(0, distance):
-        for rope in ropes:
-            rope.move_head(step_x, step_y)
-            if not rope.tail_near_head():
-                rope.move_tail()
-            rope.memorize_tail_position()
+        main_head = ropes[0]
+        main_head.move_head(step_x, step_y)
+        if not main_head.tail_near_head():
+            main_head.move_tail()
+        main_head.memorize_tail_position()
+
+        for i in range(1, len(ropes)):
+            ropes[i].head_x = ropes[i - 1].tail_x
+            ropes[i].head_y = ropes[i - 1].tail_y
+
+            if not ropes[i].tail_near_head():
+                ropes[i].move_tail()
+            ropes[i].memorize_tail_position()
     return ropes
 
 
@@ -84,7 +112,7 @@ def show_me(wts):
 
 
 def main():
-    file_name = "Day09-input-d2.txt"
+    file_name = "Day09-input-p.txt"
     data_input = parse_file(file_name)
     part_two = 1
 
@@ -97,13 +125,13 @@ def main():
         ropes = emulate_rope_motion(command.split(" "), ropes)
 
     part_one = ropes[0].get_visited_positions()
+    part_two = ropes[8].get_visited_positions()
 
     print("----------------------------")
     print("Part One:", part_one)
     print("Part Two:", part_two)
 
-    show_me(ropes[0].where_tail_was)
-
+    #show_me(ropes[0].where_tail_was)
 
 
 if __name__ == "__main__":
