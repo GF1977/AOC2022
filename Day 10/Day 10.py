5bf8d3a846261f7944418923abbeaafa3509ee2a
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-11 10:54:50 +0100

    Day 10 - started second part, added CRT and screen vizualization

--- Day 10/Day 10.py
+++ Day 10/Day 10.py
@@ -10,24 +10,45 @@ class CPU:
         self.counter = 1
         self.signal_str = 0
         self.signal_str_summary = 0
+        self.crt = []
+        for i in range(1, 241):
+            self.crt += "."
 
     def execute(self, command):
         if command == "noop":
-            self.counter += 1
-            self.counter_check()
+            self.tick()
 
         else:
             cmd, arg = command.split(" ")
-            self.counter += 1
-            self.counter_check()
+
+            pass
+            self.tick()
 
             self.X += int(arg)
-            self.counter += 1
-            self.counter_check()
+            self.tick()
+
+    def draw_sprite(self):
+        # counter position is within the 3pixel sprite (-1, 0 , 1)
+        if self.X - 1 <= self.counter <= self.X + 1:
+            self.crt[self.counter] = "#"
+
+    def show_screen(self):
+        pass
+        crt_line = ""
+        for a in self.crt:
+            crt_line+= str(a)
+        print(crt_line[ 0:40])
+        print(crt_line[40:80])
+        print(crt_line[80:120])
+        print(crt_line[120:160])
+        print(crt_line[160:200])
+        print(crt_line[200:240])
 
 
 
-    def counter_check(self):
+    def tick(self):
+        self.draw_sprite()
+        self.counter += 1
         if (self.counter + 20) % 40 == 0:
             print("Counter: ", self.counter, "     X: ", self.X)
             self.signal_str = self.X * self.counter
@@ -43,12 +64,15 @@ def main():
     for command in data_input:
         cpu.execute(command)
 
+    cpu.show_screen()
+
 
 
 
     part_one = cpu.signal_str_summary
     part_two = 1
 
+    print(" ")
     print("----------------------------")
     print("Part One:", part_one)
     print("Part Two:", part_two)
@@ -58,5 +82,5 @@ if __name__ == "__main__":
     main()
 
 # Answers:
-# Part One:
+# Part One: 15020
 # Part Two:
