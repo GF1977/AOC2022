001d9fcfec31551dd1ab215a66fd24dded3a91f3
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-11 15:18:50 +0100

    Day 11 - first part completed!

--- Day 11/Day 11.py
+++ Day 11/Day 11.py
@@ -2,27 +2,53 @@ def parse_file(file_to_process):
     file = open(file_to_process, mode="r")
     data: list[str] = file.read().split("\n")
 
-    return data
+    monkeys = []
+
+    monkey_n = 0
+    block_to_parse = []
+    block_index = 0
+
+    for line in data:
+        if "Monkey" in line:
+            block_to_parse = data[block_index: block_index + 7]
+            block_index +=7
+
+            monkey_n = block_to_parse[0].split(" ")[1][0]
+            starting_items = block_to_parse[1].split(": ")[1].split(",")
+            operation = block_to_parse[2].split("= ")[1]
+            test = block_to_parse[3].split("by ")[1]
+            if_true_to = block_to_parse[4].split("monkey ")[1]
+            if_false_to = block_to_parse[5].split("monkey ")[1]
+
+            monkeys.append(Monkey(monkey_n,starting_items, operation, test, if_true_to, if_false_to))
+            block_to_parse = []
+
+
+
+
+    return monkeys
 
 class Monkey:
-    def __init__(self, starting_items, operation, test, if_true_to_A, if_false_to_B):
+    def __init__(self, number, starting_items, operation, test, if_true_to_A, if_false_to_B):
+        self.number = int(number)
         self.starting_items = starting_items
         self.operation = operation          # new worry level = old w.l. & operation
-        self.test = test                    # divisible by test
-        self.throw_to_A = if_true_to_A      # throw to monkey A
-        self.throw_to_B = if_false_to_B     # throw to monkey B
+        self.test = int(test)                    # divisible by test
+        self.throw_to_A = int(if_true_to_A)      # throw to monkey A
+        self.throw_to_B = int(if_false_to_B)     # throw to monkey B
         self.items_inspected = 0
 
     def inspect_and_throw(self):
         throwing_items = [] # throw to, worry level      [1, 123]  w.l. 123 throw to monkey #1
         self.items_inspected += len(self.starting_items)
-        for item in self.starting_items:
+        for item_str in self.starting_items:
+            item = int(item_str)
             #print("Monkey inspects an item with a worry level of ", item)
-            worry_level = self.adjust_worry_level(int(item))
+            worry_level = self.adjust_worry_level(item)
             ops = self.operation.split(" ")
             #print("Worry level is ", ops[0]," by ", ops[1],  " to  ", worry_level)
             throw_to = self.throw_to_A
-            if worry_level % self.test:
+            if worry_level % int(self.test):
                 throw_to = self.throw_to_B
 
             #throw
@@ -40,61 +66,60 @@ class Monkey:
 
 
     def adjust_worry_level(self, level):
-        operation, value = self.operation.split(" ")
-        if value == "old":
+        old, operation, value_str = self.operation.split(" ")
+        if value_str == "old":
             value = level
+        else:
+            value = int(value_str)
 
         new_level = 0
         if operation == "+":
-            new_level = level + int(value)
+            new_level = level + value
         if operation == "*":
-            new_level = level * int(value)
+            new_level = level * value
 
-        new_level = int(new_level) // 3
+        new_level = new_level // 3
 
         return new_level
 
 def print_monkeys(monkeys: Monkey):
-    i = 0
     for monkey in monkeys:
-        items = "Monkey " + str(i) + ": "
-        i += 1
+        items = "Monkey " + str(monkey.number) + ": "
         for item in monkey.starting_items:
             items += str(item) + " "
         print(items)
 
 def inspected_items_status(monkeys: Monkey):
     i = 0
+    times_inspected = []
     for m in monkeys:
+        times_inspected.append(m.items_inspected)
         print("Monkey ", i, " inspected items ", m.items_inspected, " times.")
         i += 1
 
+    times_inspected.sort(reverse=True)
+    level_monkey_business = times_inspected[0] * times_inspected[1]
+    return level_monkey_business
 
 def main():
-    file_name = "Day11-input-d.txt"
-    data_input = parse_file(file_name)
+    file_name = "Day11-input-p.txt"
+
     part_one = 0
     part_two = 1
 
-    monkeys = [Monkey([79, 98], "* 19", 23, 2, 3),
-               Monkey([54, 65, 75, 74], "+ 6", 19, 2, 0),
-               Monkey([79, 60, 97], "* old", 13, 1, 3),
-               Monkey([74], "+ 3", 17, 0, 1)]
+    monkeys = parse_file(file_name)
 
     for round in range(1,21):
-        monkey_n = 0
         for monkey in monkeys:
-            #print("Monkey ", monkey_n, ":")
             throwing_items = monkey.inspect_and_throw()
             for item in throwing_items:
                 throw_to = item[0]
                 worry_level = item[1]
                 monkeys[throw_to].starting_items.append(worry_level)
-            monkey_n += 1
-        print("Round: ", round )
-        print_monkeys(monkeys)
+        #print("Round: ", round )
+        #print_monkeys(monkeys)
 
-    inspected_items_status(monkeys)
+    part_one = inspected_items_status(monkeys)
 
     print("----------------------------")
     print("Part One:", part_one)
