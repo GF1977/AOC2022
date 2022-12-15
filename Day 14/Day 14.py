e0b8df4a945700c4c7a7f83c8f08414c96f5d875
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-15 21:53:43 +0100

    Day 14 - first part: test example does work

--- Day 14/Day 14.py
+++ Day 14/Day 14.py
@@ -1,11 +1,11 @@
 class Map:
-    def __init__(self, max_x,  max_y, min_x, min_y):
+    def __init__(self, max_x, max_y, min_x, min_y):
         self.max_x = max_x
         self.max_y = max_y
         self.min_x = min_x
         self.min_y = min_y
         self.map = []
-
+        self.sand_units = 0
 
     def draw_line(self, a, b):
         x_a = int(a[0])
@@ -23,14 +23,58 @@ class Map:
 
         return self.map
 
-
     def draw_the_map(self):
         for y in range(0, self.max_y + 1):
             row = ""
             for x in range(self.min_x - 1, self.max_x + 1):
                 row += self.map[y][x]
             print(row)
+        print(" ")
 
+    def sand_flow(self):
+        res = 0
+        while True:
+            sand_crd = [0, 500]
+            y = sand_crd[0]
+            x = sand_crd[1]
+            res = self.roll_sand_block(y, x)
+
+            if res:
+                break
+
+
+        return self.sand_units
+
+    def roll_sand_block(self, y,x):
+        if self.sand_units % 10 == 0:
+            self.draw_the_map()
+        #print("X = ", x, "  Y = ", y)
+        if x <= self.min_x:
+            return True
+
+        repeat = True
+        while repeat:
+
+            #check below
+            if self.map[y+1][x] == ".":
+                self.roll_sand_block(y + 1,x)
+
+            # check left
+            if  self.map[y + 1][x - 1] == ".":
+                if self.roll_sand_block(y + 1,x - 1):
+                    return True
+
+            # check right
+            elif self.map[y + 1][x + 1] == ".":
+                if self.roll_sand_block(y + 1, x + 1):
+                    return True
+
+            else:
+                self.map[y][x] = "o"
+                self.sand_units+=1
+                repeat = False
+
+        return False
 
 def parse_file(file_to_process):
     file = open(file_to_process, mode="r")
@@ -40,7 +84,6 @@ def parse_file(file_to_process):
     min_y = 999
     max_x = 0
     max_y = 0
-
 
     for line in data:
         rocks = line.split(" -> ")
@@ -73,7 +116,7 @@ def parse_file(file_to_process):
         for i in range(0, len(crd) - 1):
             xy_str_a = crd[i]
             xy_str_b = crd[i + 1]
-            the_map.map = the_map.draw_line( xy_str_a.split(","), xy_str_b.split(","))
+            the_map.map = the_map.draw_line(xy_str_a.split(","), xy_str_b.split(","))
 
     the_map.map[0][500] = "+"
 
@@ -82,21 +125,13 @@ def parse_file(file_to_process):
     return the_map
 
 
-def sand_flow(the_map):
-    the_map.draw_the_map()
-    return 1
-
-
 def main():
     file_name = "Day14-Input-d.txt"
     the_map = parse_file(file_name)
 
-    part_one = sand_flow(the_map)
-
-
+    part_one = the_map.sand_flow()
 
     part_two = 1
-
 
     print("Part One:", part_one)
     print("Part Two:", part_two)
