f8d8fc06cab0a4725560ffb8a60236892d2ec3a4
Ilya Opyakin <i.opyakin@outlook.com>
2022-12-25 23:51:47 +0100

    Day 16 - decomposed main function, did some cleanup. Solution does work for both (d + p) files

--- Day 16/Day 16.py
+++ Day 16/Day 16.py
@@ -1,7 +1,6 @@
 import itertools
 import time
 
-distances = {}
 
 class Valve:
     def __init__(self, name, flow_rate, lead_to):
@@ -9,37 +8,6 @@ class Valve:
         self.flow_rate = flow_rate
         self.lead_to = lead_to
         self.is_closed = True
-        self.is_visited = False
-
-
-def walk_the_graf(valves, start_valve):
-    max_flow = -1
-    best_v = None
-    for v in start_valve.lead_to:
-        v_to_go = valves[v]
-        if v_to_go.flow_rate > max_flow and v_to_go.is_visited == False:
-            max_flow = v_to_go.flow_rate
-            best_v = v_to_go
-
-    if best_v is None:
-        return
-
-    best_v.is_visited = True
-    print(best_v.name, best_v.flow_rate)
-    walk_the_graf(valves, best_v)
-
-
-def get_next_valve(valve_from, valves):
-    #  returns obj valve which is connected to param:valve_from with the biggest flow rate
-    max_flow_rate = 0
-    valve = None
-    for direction in valve_from.lead_to:
-        candidate = valves[direction]
-        if candidate.is_closed and candidate.flow_rate > max_flow_rate:
-            max_flow_rate = candidate.flow_rate
-            valve = candidate
-
-    return valve
 
 
 def parse_file(file_to_process):
@@ -72,11 +40,10 @@ def get_distance(valves, from_valve_name, to_valve_name, steps=0):
     steps += 1
     to_go_new = []
     for v in to_go:
-        valves[v].is_visited = True
         for valve_to_go_name in valves[v].lead_to:
             if valve_to_go_name == to_valve_name:
                 return steps
-            if not valves[valve_to_go_name].is_visited:
+            if valves[valve_to_go_name].is_closed:
                 to_go_new.append(valve_to_go_name)
     result = get_distance(valves, to_go_new, to_valve_name, steps)
     return result
@@ -84,10 +51,10 @@ def get_distance(valves, from_valve_name, to_valve_name, steps=0):
 
 def unvisit_valves(valves):
     for v in valves:
-        valves[v].is_visited = False
+        valves[v].is_closed = True
 
 
-def get_total_pressure(valves, best_path):
+def get_total_pressure(valves, distances, best_path):
     minutes = 0
     current_pressure = 0
     total_pressure = 0
@@ -96,10 +63,10 @@ def get_total_pressure(valves, best_path):
     for i in range(0, len(best_path) - 1):
         v_from = best_path[i]
         v_to = best_path[i + 1]
-        distance = get_distance(valves, v_from, v_to)
-        unvisit_valves(valves)
-        if distance > 4:
-            return 0, []
+        distance = distances[v_from+v_to]
+
+        # if distance > 10:
+        #     break
 
         if minutes + distance + 1 > 30:
             break
@@ -119,76 +86,52 @@ def get_total_pressure(valves, best_path):
     return total_pressure, opened_valves
 
 
-def main():
+def get_the_maximum_pressure(valves, distances,  flow_rate_to_ignore=0, path_len=6):
     start_time = time.time()
-    file_name = "Day16-Input-p.txt"
-    valves = parse_file(file_name)
-    part_one = 0
-    part_two = 1
-
-
-    # for v1 in valves:
-    #     for v2 in valves:
-    #         if v1 != v2 and valves[v1].flow_rate > 0 and valves[v2].flow_rate > 0:
-    #             key = v1 + v2
-    #             distances[key] = get_distance(valves, v1, v2)
-
     valves_with_flow = []
     for v in valves:
-        if valves[v].flow_rate > 4:
-            #print(v, valves[v].flow_rate)
+        if valves[v].flow_rate > flow_rate_to_ignore:
             valves_with_flow.append(v)
 
     best_pressure = 0
 
-    # valves_with_flow.remove('YL')
-    #valves_with_flow.remove('FF')
-    #valves_with_flow.remove('UZ')
-    #valves_with_flow.remove('TU')
-    #valves_with_flow.remove('QZ')
-    valves_with_flow.remove('CU')
-    valves_with_flow.remove('IZ')
+    all_paths = itertools.permutations(valves_with_flow, path_len)
 
-    print(len(valves_with_flow))
-    all_paths = itertools.permutations(valves_with_flow, 8)
-
-    counter = 0
     for best_path in all_paths:
         a = list(best_path)
-        # a.insert(0, 'YL')
-        #a.insert(0, 'FF')
-        #a.insert(0, 'UZ')
-        #a.insert(0, 'TU')
-        #a.insert(0, 'QZ')
-        a.insert(0, 'CU')
-        a.insert(0, 'IZ')
-
-
         a.insert(0, 'AA')
         best_path = a
-        o_v = []
-        res = 0
 
-        res, o_v = get_total_pressure(valves, best_path)
-        #print(best_path, res)
+        res, o_v = get_total_pressure(valves, distances, best_path)
         if res > best_pressure:
             best_pressure = res
             print(best_path, o_v, res)
-            print(" Processed: %i --- %s seconds ---" % (counter, time.time() - start_time))
-        counter+=1
-    #walk_the_graf(valves, valves['AA'])
+            print("--- %s seconds ---" % (time.time() - start_time))
+    return best_pressure
 
-    #best_path = ['AA', 'DD', 'BB', 'JJ', 'HH', 'EE', 'CC']
-    best_path = ['AA', 'IZ', 'CU', 'QZ', 'TU', 'UZ', 'FF', 'GG', 'ZL']
-                # ['AA', 'IZ', 'CU', 'QZ', 'TU', 'UZ', 'FF', 'GG', 'ZL'] Winner!
-                #['AA', 'TU', 'QZ', 'CU', 'IZ', 'PA', 'XF', 'YL', 'JH', 'OI', 'GG', 'ZL', 'UZ', 'TR', 'SZ', 'FF']
-    part_two = get_total_pressure(valves, best_path)
 
-    #distance = get_distance(valves, 'HH', 'EE')
-    #print(distance)
+def calculate_all_distances(valves):
+    result = {}
+    for v1 in valves:
+        for v2 in valves:
+            if v1 != v2 and valves[v1].flow_rate > 0 and valves[v2].flow_rate > 0 or v1 == 'AA':
+                result[v1 + v2] = get_distance(valves, v1, v2)
+                unvisit_valves(valves)
+    return result
 
+def main():
+    start_time = time.time()
+    file_name = "Day16-Input-d.txt"
+    valves = parse_file(file_name)
+
+    distances = calculate_all_distances(valves)
+    part_one = get_the_maximum_pressure(valves, distances)
+
+    part_two = 1
+
+    print("--- %s seconds ---" % ( time.time() - start_time))
     print("----------------------------")
-    print("Part One:", best_pressure)
+    print("Part One:", part_one)
     print("Part Two:", part_two)
 
 
