b472f6842a9c7ecf5639d0a78299aaecd59c15ab
Ilya Opyakin <i.opyakin@outlook.com>
2023-03-16 22:59:56 +0100

    Day 16 - final solution is ready! 34 seconds.
    Added def get_permutations(L, N): to predefine all the valid permutations.

--- Day 16/Day 16.py
+++ Day 16/Day 16.py
@@ -1,7 +1,7 @@
-import itertools
-import time
-import re
 import collections
+import itertools
+import re
+import time
 
 
 class Valve:
@@ -59,11 +59,9 @@ def calculate_all_distances(valves):
 
 
 def breadth_first_search(valves, available_valves, distances, rest_minutes):
-    current_pressure = 0
-    total_pressure = 0
     max_pressure = 0
     max_path = []
-    queue = collections.deque([("AA", available_valves, rest_minutes, current_pressure, total_pressure, ["AA"])])
+    queue = collections.deque([("AA", available_valves, rest_minutes, 0, 0, ["AA"])])
 
     while queue:
         current_valve, available_valves, rest_minutes, current_pressure, total_pressure, path = queue.popleft()
@@ -73,9 +71,7 @@ def breadth_first_search(valves, available_valves, distances, rest_minutes):
                 exec_time = (distances[current_valve + v] + 1)
 
                 if exec_time <= rest_minutes:
-                    saved_m = rest_minutes
-                    saved_c = current_pressure
-                    saved_t = total_pressure
+                    stash = (rest_minutes, current_pressure, total_pressure)
 
                     rest_minutes -= exec_time
                     total_pressure += exec_time * current_pressure
@@ -84,9 +80,7 @@ def breadth_first_search(valves, available_valves, distances, rest_minutes):
                     queue.append((v, list(available_valves)[:i] + list(available_valves)[i + 1:], rest_minutes,
                                   current_pressure, total_pressure, path + [v]))
 
-                    current_pressure = saved_c
-                    total_pressure = saved_t
-                    rest_minutes = saved_m
+                    rest_minutes, current_pressure, total_pressure = stash
 
         if rest_minutes > 0:
             total_pressure += rest_minutes * current_pressure
@@ -98,33 +92,15 @@ def breadth_first_search(valves, available_valves, distances, rest_minutes):
     return max_pressure, max_path
 
 
-def get_path_cost(my_path, distances):
-    path_cost = 0
-    for i in range(0, len(my_path) - 1):
-        v1 = my_path[i]
-        v2 = my_path[i + 1]
-        path_cost += distances[v1 + v2]
+def get_permutations(L, N):
+    # Get all permutations of size N
+    perms = itertools.permutations(L, N)
 
-    return path_cost
-
-
-def get_pressure_for_path(valves, path, distances, rest_minutes):
-    current_valve = "AA"
-    total_pressure = 0
-    current_pressure = 0
-    for v in path:
-        if v == "AA":
-            continue
-        exec_time = (distances[current_valve + v] + 1)
-        if exec_time <= rest_minutes:
-            rest_minutes -= exec_time
-            total_pressure += exec_time * current_pressure
-            current_pressure += valves[v].flow_rate
-            current_valve = v
-    if rest_minutes > 0:
-        total_pressure += rest_minutes * current_pressure
-
-    return total_pressure
+    # Filter out permutations with duplicates
+    unique_perms = set()
+    for perm in perms:
+        unique_perms.add(tuple(sorted(perm)))
+    return unique_perms
 
 
 def main():
@@ -133,52 +109,46 @@ def main():
     valves = parse_file(file_name)
     distances = calculate_all_distances(valves)
 
-    part_one = 0
-    part_two = 0
+    minimal_flow_rate = min((v.flow_rate for v in valves.values() if v.flow_rate > 0), default=0)
+    # it is a trick, we remove at least one valve to decrease the amount of permutations
+    # if it doesn't work, uncomment the following line
+    # minimal_flow_rate = 0
 
-    valves_with_flow = [key for key, value in valves.items() if value.flow_rate > 0]
+    valves_with_flow = [key for key, value in valves.items() if value.flow_rate > minimal_flow_rate]
 
+    # Part one
     # 30 minutes is hardcoded requirement for this scenario
     best_res, best_path = breadth_first_search(valves, valves_with_flow, distances, rest_minutes=30)
     print(best_res, best_path)
     print("--- %s seconds ---" % (time.time() - start_time))
 
-    cost = get_pressure_for_path(valves, best_path, distances, rest_minutes=30)
-    print("Cost for: ", best_path, " = ", cost)
-
-
     # Part two
 
-    all_my_paths = itertools.permutations(valves_with_flow, len(valves_with_flow) // 2)
-    best_res = 0
-    # for 7 it will be  =  15!/(15-7)! = 32,432,400
-
+    all_my_paths = get_permutations(valves_with_flow, len(valves_with_flow) // 2)
+    # for 7 out of 15 it will be  =  15! / (7! * (15-7)!) = 6435
     for my_path in all_my_paths:
-
         el_path = list(set(valves_with_flow).difference(set(my_path)))
 
         # 26 minutes is hardcoded requirement for this scenario
         res_my, best_path_my = breadth_first_search(valves, my_path, distances, rest_minutes=26)
-
         res_el, best_path_el = breadth_first_search(valves, el_path, distances, rest_minutes=26)
+
         if (res_my + res_el) > best_res:
             print(best_path_my, best_path_el, "    Res = ", res_my, "+", res_el, "=", res_my + res_el)
             print("--- %s seconds ---" % (time.time() - start_time))
             best_res = res_my + res_el
 
-    print("--- %s seconds ---" % (time.time() - start_time))
     print("----------------------------")
-    print("Part One:", part_one)
-    print("Part Two:", part_two)
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == "__main__":
     main()
 
 # Answers:
-# Part One: 1641 1.70 seconds
-# Part Two: 2261 3.79 seconds
+# Part One: 1641
+# Part Two: 2261
 
 # Answers P2:
-# Part One: 1584  1.49 seconds
-# Part Two: 2052  5.92 seconds
+# Part One: 1584
+# Part Two: 2052
